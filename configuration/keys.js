e13a053c19874eb0d011b7bae89a35d6346d4767
waynehuynhz <85436146+waynehuynhz@users.noreply.github.com>
2021-07-02 00:24:15 -0700

    Update keys.js

--- configuration/keys.js
+++ configuration/keys.js
@@ -1,8 +1,8 @@
 // https://developer.twitter.com/
 
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
+  consumer_key: 'SQqYQpVlBLXxgph7FlEOxgZxM',
+  consumer_secret: 'F26JMihp5XNBi9QzJhmcs8wnOGC6GVyashNKoQUCtW4JuslSiw',
+  access_token: '1401772748049764353-NZuDWI5ypxdRd6EqlW6Inin0W0VkV4',
+  access_token_secret: 'tNXNkyFf2smTIPHCzLE8GYPj89RIwvYybIgvU7XeoLs6c',
 };
